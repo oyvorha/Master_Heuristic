e7d66b7e6f998042e4f8749a7b426da9f67653e2
oyvorsh <oyvorsh@stud.ntnu.no>
2020-03-04 10:03:20 +0100

    Add vehicle object

---
+++ vehicle.py
@@ -0,0 +1,20 @@
+
+class Vehicle:
+
+    def __init__(self, init_battery_load, init_battery_bikes, init_flat_bikes, cap=20):
+        self.current_batteries = init_battery_load
+        self.current_battery_bikes = init_battery_bikes
+        self.current_flat_bikes = init_flat_bikes
+        self.capacity = cap
+
+    def change_battery_bikes(self, bikes):
+        self.current_battery_bikes -= bikes
+
+    def change_flat_bikes(self, bikes):
+        self.current_flat_bikes -= bikes
+
+    def swap_batteries(self, batteries):
+        self.current_batteries -= batteries
+
+    def available_capacity(self):
+        return max(0, self.capacity - (self.current_flat_bikes + self.current_battery_bikes))
