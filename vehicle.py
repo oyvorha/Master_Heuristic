208acf706dfe275ef701d98db363fc1ee7d07e76
oyvorsh <oyvorsh@stud.ntnu.no>
2020-03-20 14:15:05 +0100

    Update vehicle object names and functions

--- vehicle.py
+++ vehicle.py
@@ -1,21 +1,29 @@
 
 class Vehicle:
 
-    def __init__(self, init_battery_load, init_battery_bikes, init_flat_bikes, cap=20, bat_cap=20):
+    def __init__(self, init_battery_load, init_charged_bikes, init_flat_bikes, bike_cap=20, bat_cap=20):
         self.current_batteries = init_battery_load
-        self.current_battery_bikes = init_battery_bikes
+        self.current_charged_bikes = init_charged_bikes
         self.current_flat_bikes = init_flat_bikes
-        self.bike_capacity = cap
+        self.bike_capacity = bike_cap
         self.battery_capacity = bat_cap
 
     def change_battery_bikes(self, bikes):
-        self.current_battery_bikes -= bikes
+        self.current_charged_bikes += bikes
+        if self.current_charged_bikes < 0:
+            self.current_charged_bikes = 0
+        if self.current_charged_bikes + self.current_flat_bikes > self.bike_capacity:
+            self.current_charged_bikes = self.bike_capacity - self.current_flat_bikes
 
     def change_flat_bikes(self, bikes):
-        self.current_flat_bikes -= bikes
+        self.current_flat_bikes += bikes
+        if self.current_flat_bikes < 0:
+            self.current_flat_bikes = 0
+        if self.current_charged_bikes + self.current_flat_bikes > self.bike_capacity:
+            self.current_flat_bikes = self.bike_capacity - self.current_charged_bikes
 
     def swap_batteries(self, batteries):
-        self.current_batteries -= batteries
+        self.current_batteries = max(0, self.current_batteries - batteries)
 
-    def available_capacity(self):
-        return max(0, self.bike_capacity - (self.current_flat_bikes + self.current_battery_bikes))
+    def available_bike_capacity(self):
+        return max(0, self.bike_capacity - (self.current_flat_bikes + self.current_charged_bikes))
