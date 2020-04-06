0d7fed283b76c09c04706b9e0a9e80a413c6e6d0
oyvorsh <oyvorsh@stud.ntnu.no>
2020-04-06 12:28:15 +0200

    Update subproblem manager

--- Subproblem/model_manager.py
+++ Subproblem/model_manager.py
@@ -1,4 +1,3 @@
-from Subproblem.generate_route_pattern import GenerateRoutePattern
 from Subproblem.parameters_subproblem import ParameterSub
 from Subproblem.subproblem_model import run_model
 
@@ -7,53 +6,46 @@ class ModelManager:
 
     time_horizon = 25
 
-    def __init__(self, start_station, all_stations, scenarios, vehicle):
-        self.starting_station = start_station
+    def __init__(self, vehicle):
         self.vehicle = vehicle
-        self.gen = GenerateRoutePattern(self.starting_station, all_stations, vehicle)
-        self.gen.get_columns()
-        self.customer_scenarios = scenarios
+        self.scores = list()
 
-    def run_all_subproblems(self):
-        for route in self.gen.finished_gen_routes:
-            for pattern in self.gen.patterns:
-                for customer_scenario in self.customer_scenarios:
-                    self.run_one_subproblem(route, pattern, customer_scenario)
-
-    def run_one_subproblem(self, route, pattern, customer_scenario):
-        customer_arrivals = ModelManager.arrivals_after_visit(route, customer_scenario)
+    def run_one_subproblem(self, route, route_full_set_index, pattern, customer_scenario):
+        customer_arrivals = ModelManager.arrivals_after_visit(route, route_full_set_index, customer_scenario)
         L_CS = list()
         L_FS = list()
         base_viol = list()
         for i in range(len(route.stations)):
-            st_L_CS, st_L_FS = self.get_base_inventory(route.stations[i], route.station_visits[i], customer_scenario)
+            st_L_CS, st_L_FS = ModelManager.get_base_inventory(route.stations[i], route.station_visits[i],
+                                                               customer_scenario[route_full_set_index[i]])
             st_viol = ModelManager.get_base_violations(route.stations[i], st_L_CS, st_L_FS, customer_arrivals[i])
             L_CS.append(st_L_CS)
             L_FS.append(st_L_FS)
             base_viol.append(st_viol)
         params = ParameterSub(route, self.vehicle, pattern, customer_arrivals, L_CS, L_FS, base_viol)
-        run_model(params)
+        return run_model(params)
 
     """
     Returns number of customer arrivals for incoming charged bikes, incoming flat bikes, outgoing charged bikes 
     from time of visit to time horizon based on scenario
     """
     @staticmethod
-    def arrivals_after_visit(route, customer_arrivals):
+    def arrivals_after_visit(route, route_ind, customer_arrivals):
         arrivals = list()
-        for i in range(len(route.stations)):
+        for i in range(len(route_ind)):
             arrival_time = route.station_visits[i]
             if arrival_time > ModelManager.time_horizon:
                 arrivals.append([0, 0, 0])
             else:
+                time_station_arrivals = customer_arrivals[route_ind[i]]
                 st_arrivals = list()
-                for j in range(len(customer_arrivals)):
-                    split_index = len(customer_arrivals) - 1
-                    for k in range(len(customer_arrivals[j])):
-                        if customer_arrivals[j][k] > arrival_time:
+                for j in range(len(time_station_arrivals)):
+                    split_index = len(time_station_arrivals) - 1
+                    for k in range(len(time_station_arrivals[j])):
+                        if time_station_arrivals[j][k] > arrival_time:
                             split_index = k
                             break
-                    counted_arrivals = customer_arrivals[j][split_index:]
+                    counted_arrivals = time_station_arrivals[j][split_index:]
                     st_arrivals.append(len(counted_arrivals))
                 arrivals.append(st_arrivals)
         return arrivals
