07a166424a9b559baf79db6cbf3caa8a44613cdb
oyvorsh <oyvorsh@stud.ntnu.no>
2020-04-16 10:43:40 +0200

    Update vehicle object to include id and print pattern in visualization

--- vehicle.py
+++ vehicle.py
@@ -2,7 +2,8 @@
 class Vehicle:
 
     def __init__(self, init_battery_load, init_charged_bikes, init_flat_bikes, current_station,
-                 bike_cap=20, bat_cap=20):
+                 bike_cap=20, bat_cap=20, id=0):
+        self.id = id
         self.current_station = current_station
         self.current_batteries = init_battery_load
         self.current_charged_bikes = init_charged_bikes
@@ -15,14 +16,14 @@ class Vehicle:
         if self.current_charged_bikes < 0:
             self.current_charged_bikes = 0
         if self.current_charged_bikes + self.current_flat_bikes > self.bike_capacity:
-            self.current_charged_bikes = self.bike_capacity - self.current_flat_bikes
+            print("Capacity overflow")
 
     def change_flat_bikes(self, bikes):
         self.current_flat_bikes += bikes
         if self.current_flat_bikes < 0:
             self.current_flat_bikes = 0
         if self.current_charged_bikes + self.current_flat_bikes > self.bike_capacity:
-            self.current_flat_bikes = self.bike_capacity - self.current_charged_bikes
+            print("Capacity overflow")
 
     def swap_batteries(self, batteries):
         self.current_batteries = max(0, self.current_batteries - batteries)
