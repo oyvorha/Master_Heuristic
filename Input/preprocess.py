b42ad55d80d0d5f60a6f69d72c3bbee936608e50
oyvorsh <oyvorsh@stud.ntnu.no>
2020-04-23 10:22:47 +0200

    Include charging stations when initializing system

--- Input/preprocess.py
+++ Input/preprocess.py
@@ -15,17 +15,27 @@ def generate_all_stations(scenario):
 
     station_objects = []
     for id, station in stations.items():
+
+        battery_rate = 0.5
+        flat_rate = 0.5
+        charging_station = False
+
+        if int(id) % 5 == 0:
+            battery_rate = 1
+            flat_rate = 0
+            charging_station = True
+
         latitude = float(station[0])
         longitude = float(station[1])
-        init_battery_load = round(station[2][scenario][0], 0)
-        init_flat_load = round(station[2][scenario][1], 0)
-        incoming_charged_bike_rate = station[2][scenario][2]
-        incoming_flat_bike_rate = station[2][scenario][3]
-        outgoing_charged_bike_rate = station[2][scenario][4]
+        init_battery_load = round(battery_rate * station[2][scenario][0], 0)
+        init_flat_load = round(flat_rate * station[2][scenario][0], 0)
+        incoming_charged_bike_rate = battery_rate * station[2][scenario][1]
+        incoming_flat_bike_rate = flat_rate * station[2][scenario][1]
+        outgoing_charged_bike_rate = station[2][scenario][2]
         ideal_state = 10
         obj = Station(latitude, longitude, init_battery_load, init_flat_load
                       , incoming_charged_bike_rate, incoming_flat_bike_rate, outgoing_charged_bike_rate, ideal_state,
-                      id)
+                      id, charging=charging_station)
         station_objects.append(obj)
     return station_objects
 
